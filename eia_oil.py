4456020109ff8ff4dc6797248cc4bd2aabba96bb
Jamith Nimantha <jamithnimantha@gmail.com>
2021-09-24 19:30:39 +0530

    Initial commit

---
+++ eia_oil.py
@@ -0,0 +1,53 @@
+import requests
+from datetime import datetime
+import os
+import time
+import xlrd
+import shutil
+
+error = '503 - Sorry! This file is temporarily unavailable.'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+current = datetime.now().strftime('OIL_%Y-%m-%d.xls')
+start_time = time.time()
+
+# CHANGE WAIT TIME BELOW
+wait_sleep_time = 10
+
+path = f'{dir_path}{os.sep}Data{os.sep}Oil{os.sep}'
+if not os.path.exists(path):
+    os.makedirs(path)
+
+
+def is_valid(content):
+    temp_folder = f'{dir_path}{os.sep}.temp{os.sep}'
+    if not os.path.exists(temp_folder):
+        os.mkdir(temp_folder)
+    open(temp_folder + current, 'wb').write(content)
+    workbook = xlrd.open_workbook(temp_folder + current)
+    worksheet = workbook.sheet_by_index(0)
+    first_row_value = worksheet.row(1)[0].value
+    if first_row_value == error:
+        print(worksheet.row(1)[0].value)
+        shutil.rmtree(temp_folder)
+        return False
+    shutil.rmtree(temp_folder)
+    return True
+
+
+while True:
+    try:
+        content = requests.get('http://ir.eia.gov/wpsr/psw01.xls', allow_redirects=True).content
+        if is_valid(content):
+            open(path + current, 'wb').write(content)
+            print(f'Found {path + current} with Data !!!')
+            break
+        print(f'Trying again in {wait_sleep_time} seconds!')
+        time.sleep(wait_sleep_time)
+    except:
+        print("Trying Again in 5 Seconds")
+        if time.time() - start_time > 600:
+            print('10 minutes have passed exiting')
+            exit()
+        time.sleep(wait_sleep_time)
+        continue
+exit()
